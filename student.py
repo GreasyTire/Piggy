ab0ab2dc22bfdf01ba6202e3dfea66a922361543
SpoiledMilk62 <125601556+GreasyTire@users.noreply.github.com>
2023-04-13 19:02:40 +0000

    added menu item

--- student.py
+++ student.py
@@ -40,7 +40,8 @@ class Piggy(PiggyParent):
                 "s": ("Shy", self.shy),
                 "f": ("Follow", self.follow),
                 "c": ("Calibrate", self.calibrate),
-                "q": ("Quit", self.quit)
+                "q": ("Quit", self.quit),
+                "l": ("Luke", self.)
                 }
         # loop and print the menu...
         for key in sorted(menu.keys()):
@@ -55,6 +56,9 @@ class Piggy(PiggyParent):
     STUDENT PROJECTS
     ****************
     '''
+
+    def Luke(self):
+      print("This is for testing")
 
     def dance(self):
         """A higher-ordered algorithm to make your robot dance"""
