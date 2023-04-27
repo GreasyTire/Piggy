0a434c0a963a82eeddcc2fde3d6be6a131c4496f
GreasyTire <125601556+GreasyTire@users.noreply.github.com>
2023-04-27 18:40:54 +0000

    start dance thing

--- student.py
+++ student.py
@@ -84,9 +84,23 @@ class Piggy(PiggyParent):
         # TODO: check to see if it's safe before dancing
         
         # lower-ordered example...
-        self.right(primary=50, counter=50)
-        time.sleep(2)
-        self.stop()
+        for x in range(3):
+          self.right()
+          time.sleep(.4)
+          self.stop()
+
+          self.left()
+          time.sleep(.4)
+          self.stop()
+
+        for x in range(3):
+          self.fwd()
+          time.sleep(.7)
+          self.stop()
+
+          self.back()
+          time.sleep(.7)
+          self.stop()
 
     def safe_to_dance(self):
         """ Does a 360 distance check and returns true if safe """
